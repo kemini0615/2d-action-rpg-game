3ddf412a903b092bd83fc92af39d8764eb131122
kemini <moeday1010@gmail.com>
2025-11-29 09:43:29 +0900

    Player 클래스의 상태 머신을 public에서 private으로 수정

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // 플레이어는 자신의 상태 머신을 갖는다.
-    public StateMachine StateMachine { get; private set; }
+    private StateMachine stateMachine;
 
     // 플레이어는 상태 머신의 현재 상태를 설정하기 위해서 자신의 다양한 상태를 갖는다.
     public PlayerIdleState IdleState { get; private set;} // 정지 상태.
@@ -11,19 +11,19 @@ public class Player : MonoBehaviour
 
     void Awake()
     {
-        StateMachine = new StateMachine();
-        IdleState = new PlayerIdleState(this, StateMachine, "Idle");
-        MoveState = new PlayerMoveState(this, StateMachine, "Move");
+        stateMachine = new StateMachine();
+        IdleState = new PlayerIdleState(this, stateMachine, "Idle");
+        MoveState = new PlayerMoveState(this, stateMachine, "Move");
     }
     void Start()
     {
         // 상태 머신 초기화.
-        StateMachine.Initialize(IdleState);
+        stateMachine.Initialize(IdleState);
     }
 
     void Update()
     {
         // 상태 머신의 현재 상태 유지.
-        StateMachine.CurrentState.Update();
+        stateMachine.CurrentState.Update();
     }
 }
