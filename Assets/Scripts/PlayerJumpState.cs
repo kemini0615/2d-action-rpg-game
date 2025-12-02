57df4a8577c223ed5a4aa6d4499f8cde5d77b32b
kemini <moeday1010@gmail.com>
2025-12-02 21:50:20 +0900

    하나의 프레임에 2번의 트랜지션이 발생하는 버그 수정

--- Assets/Scripts/PlayerJumpState.cs
+++ Assets/Scripts/PlayerJumpState.cs
@@ -13,7 +13,10 @@ public class PlayerJumpState : PlayerAirState
     {
         base.Update();
 
-        if (player.Rigidbody.linearVelocity.y < 0)
+        // 한 프레임 안에서 2개의 트랜지션이 발생하는 버그 수정.
+            // 1. Air(Jump) 상태 → JumpAttack 상태
+            // 2. Jump 상태 → Fall 상태
+        if (player.Rigidbody.linearVelocity.y < 0 && stateMachine.CurrentState != player.JumpAttackState)
             stateMachine.ChangeState(player.FallState); // Fall 상태로 트랜지션.
     }
 
