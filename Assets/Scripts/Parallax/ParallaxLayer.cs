e17558c4147be6d36bca114e68b59e24371b18fd
kemini <moeday1010@gmail.com>
2025-12-07 11:37:30 +0900

    무한하게 연속되는 배경 구현 및 지터링(Jittering) 현상 수정

--- Assets/Scripts/Parallax/ParallaxLayer.cs
+++ Assets/Scripts/Parallax/ParallaxLayer.cs
@@ -5,9 +5,33 @@ public class ParallaxLayer
 {
     [SerializeField] private float parallaxMultiplier;
     [SerializeField] private Transform layer;
+    private float offset = 10f;
+
+    private float halfWidth;
+
+    public void Init()
+    {
+        halfWidth = layer.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+    }
 
     public void Move(float distance)
     {
         layer.position += new Vector3(distance * parallaxMultiplier, 0, 0);
+    }
+
+    public void Loop(float cameraLeftLimit, float cameraRightLimit)
+    {
+        float layerLeftLimit = layer.position.x - halfWidth + offset;
+        float layerRightLimit = layer.position.x + halfWidth - offset;
+
+        if (layerRightLimit < cameraLeftLimit)
+        {
+            layer.position += new Vector3(halfWidth * 2, 0, 0);
+        }
+        else if(layerLeftLimit > cameraRightLimit)
+        {
+            layer.position -= new Vector3(halfWidth * 2, 0, 0);
+        }
+        
     }
 }
--- Assets/Scripts/Parallax/ParallaxManager.cs
+++ Assets/Scripts/Parallax/ParallaxManager.cs
@@ -5,23 +5,40 @@ public class ParallaxController : MonoBehaviour
     [SerializeField] private ParallaxLayer[] layers;
 
     private Camera mainCamera;
+    private float cameraHalfWidth;
     private float lastPositionX;
 
     void Awake()
     {
         mainCamera = Camera.main;
         lastPositionX = mainCamera.transform.position.x;
+
+        // orthographicSize: 카메라의 세로 절반(half-height).
+        // aspect: 카메라의 가로 / 세로 비율.
+        // orthographicSize * aspect: 카메라의 가로 절반(half-width).
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect;
+
+        foreach (ParallaxLayer layer in layers)
+        {
+            layer.Init();
+        }
     }
 
-    void Update()
+    // 카메라를 따라 움직일 때 float 연산의 오차로 인해 지터링(Jittering) 발생.
+    // 이를 막기 위해 FixedUpdate() 메소드로 변경.
+    void LateUpdate()
     {
         float currentPositionX = mainCamera.transform.position.x; // 카메라의 현재 위치.
         float distance = currentPositionX - lastPositionX; // 카메라의 이동 거리.
         lastPositionX = currentPositionX; // 카메라의 마지막 위치 갱신.
 
+        float cameraLeftLimit = currentPositionX - cameraHalfWidth; // 카메라의 왼쪽 끝.
+        float cameraRightLimit = currentPositionX + cameraHalfWidth; // 카메라의 오른쪽 끝.
+
         foreach (ParallaxLayer layer in layers)
         {
             layer.Move(distance);
+            layer.Loop(cameraLeftLimit, cameraRightLimit);
         }
     }
 }
